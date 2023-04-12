f3f450e89a064f222b08156070614ed3954a0e07
AlphaTab435 <officeusesep@gmail.com>
2023-04-12 11:10:00 +0500

    init commit

---
+++ src/utils/geoCode.js
@@ -0,0 +1,27 @@
+const request=require('request');
+
+const geocode = function(locationName,callBack){
+    const urlGeocoding=`http://api.positionstack.com/v1/forward?access_key=73e333d043b418ee8cd13e16e2deb137&query="${locationName}"&limit=1`;
+    request({url: urlGeocoding, json: true}, function(error,response,body){
+        if(error){  
+            callBack(`Can not connect to ${error.hostname}, undefined`);
+        } else if(response.body.error || response.body.data.length==0){
+            callBack("Can not find location",undefined);
+        }
+        else{
+        // console.log(chalk.green.inverse(`Location Found`));
+        const lat=response.body.data[0].latitude;
+        const long=response.body.data[0].longitude;
+        const lable=response.body.data[0].label;
+        const locationObj={
+            lat: lat,
+            long: long,
+            location: lable,
+        }
+        callBack(undefined,locationObj);
+        }
+
+    });
+}
+
+module.exports=geocode;
